92ac0ea346d5350f55ff3a820a16bc4e9a4551fc
ertembiyik <ertembiyik@gmail.com>
2022-02-16 19:15:34 +0300

    Initial Commit

---
+++ Web3Swift/Web3SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Web3SwiftApp.swift
+//  Web3Swift
+//
+//  Created by Ertem Biyik on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Web3SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
